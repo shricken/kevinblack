532249a648bc7b80e0f4000aed17562b48bcb467
Kevin Black <kevinchristopherblack@gmail.com>
2025-01-31 01:40:16 -0800

    animation tweaks for icon carousel.

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -22,7 +22,16 @@ export const Icon: React.FC<{
   return (
     <div>
       {media && typeof media !== 'string' && (
-        <motion.div animate={{ y: isActive ? -15 : 0 }}>
+        <motion.div
+          animate={{ y: isActive ? -15 : 0 }}
+          transition={{
+            type: 'spring',
+            bounce: 0.5,
+            duration: 0.5,
+            delay: isActive ? 0.05 : 0,
+            ease: [0.215, 0.61, 0.355, 1],
+          }}
+        >
           <Media
             imgClassName={cn('w-16 h-16 mx-2', media?.classes)}
             resource={media}
